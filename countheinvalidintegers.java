5930b681baa997c7cedc11399a3d29c0acd6101b
Abhishek Kanojia <abhishek14kanojia@gmail.com>
2022-09-08 19:58:09 +0530

    Day1-assigment

---
+++ countheinvalidintegers.java
@@ -0,0 +1,24 @@
+
+//public class countheinvalidintegers {
+
+	//public static void main(String[] args) {
+		
+	//	int a = Integer.parseInt(args[0]);
+	//	int b = Integer.parseInt(args[1]);
+	//	
+	//	int sum = a+b;
+		
+	//	System.out.println("sum " +sum);
+
+//	}
+
+//}
+
+public class countheinvalidintegers {
+    public static void main(String[] args) {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int sum = a + b;
+        System.out.println("Sum is " + sum);
+    }
+}
---
+++ equalornot.java
@@ -0,0 +1,13 @@
+
+public class equalornot {
+
+	public static void main(String[] args) {
+		String a= "1";
+		String b = "2";
+		String c = "1";
+		
+			System.out.println(a == b);
+			System.out.println(a == c);
+	}
+
+}
